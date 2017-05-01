559f2bb4083e240c32cff9cc4e451ee1c4e4eb5e
Daniel Sperry <danny.sperry@gmail.com>
2017-05-01 11:31:14 -0700

    Updating v1 api_controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,4 @@
-module Api::V1
-  class ApiController < ApplicationController
-    before_action :authenticate_user!
-    respond_to :json
-  end
+class Api::V1::ApiController < ApplicationController
+  before_action :authenticate_user!
+  respond_to :json
 end
