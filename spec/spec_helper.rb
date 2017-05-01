a818e6ececad556be769aa73b8c4ac6fdabef364
Daniel Sperry <danny.sperry@gmail.com>
2017-05-01 11:34:38 -0700

    Adding serializers group to simplecov results

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ SimpleCov.start 'rails' do
   add_filter 'app/mailers/application_mailer.rb'
   add_filter 'app/models/application_record.rb'
   add_filter 'app/channels/application_cable'
+
+  add_group 'Serializers', 'app/serializers'
 end
 SimpleCov.minimum_coverage 90
 
